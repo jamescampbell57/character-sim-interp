a951439f3e18ad583a977c7afee2456888720077
ARENA Account <pranav@arena.education>
2023-06-10 17:26:11 +0100

    tiny stories/transformerlens support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # pip install sentencepiece
 
 #%%
@@ -6,10 +7,14 @@ from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch
 from huggingface_hub import snapshot_download
 
+from transformer_lens import HookedTransformer
+
 checkpoint_location = snapshot_download("decapoda-research/llama-13b-hf")
 
 with init_empty_weights():  # Takes up near zero memory
     model = LlamaForCausalLM.from_pretrained(checkpoint_location)
+
+# %%
 
 model = load_checkpoint_and_dispatch(
     model,
@@ -24,14 +29,21 @@ tok = LlamaTokenizer.from_pretrained(checkpoint_location)
 
 
 #%%
-text = """Mary: I went to the doctor's the other day.
-John: What did he say?"""
+text = '''Mary is a bookish and introverted historian with a sharp wit. John is an adventurous, outgoing photographer with a passion for extreme sports.
+
+Mary: "John, how many times have I told you not to leave your rock-climbing gear in the study? This is a space of learning and solitude, not an extreme sports store."
+
+Jasper: "Ah, come on, Mary. Can't you see how this gear tells its own history? Now, that's a story!"'''
+
 
 token_ids = tok(text, return_tensors="pt").to(model.device)
 output = model.generate(
     **token_ids,
-    max_new_tokens=60,
+    max_new_tokens=200,
 )
 
 #%%
 print(tok.batch_decode(output)[0])
+
+
+# %%
---
+++ main2.py
@@ -0,0 +1,69 @@
+# %%
+import os
+import sys
+tl_path = f"{os.getcwd()}/TransformerLens"
+sys.path.insert(0, tl_path)
+
+import torch
+from jaxtyping import Float
+from tqdm import tqdm
+from IPython.display import display
+from transformer_lens.loading_from_pretrained import get_pretrained_model_config
+from transformer_lens.hook_points import HookPoint
+from transformer_lens import HookedTransformer
+from transformer_lens import utils, HookedTransformer, HookedTransformerConfig, ActivationCache
+import circuitsvis as cv
+from huggingface_hub import snapshot_download
+from transformers import LlamaTokenizer
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# %%
+#checkpoint_location = snapshot_download("decapoda-research/llama-7b-hf")
+#tok = LlamaTokenizer.from_pretrained(checkpoint_location)
+#tok.add_special_tokens({'pad_token': '[PAD]'})
+#cfg = get_pretrained_model_config(model_name="llama-7b-hf")
+#cfg.dtype = torch.float16
+#model = HookedTransformer(cfg, tok)
+
+model = HookedTransformer.from_pretrained("roneneldan/TinyStories-2Layers-33M")
+
+# %%
+
+text = '''Mary: "Have you seen the latest Spiderman movie?"
+John: "No, how is it?"
+Mary: The acting was great, but the plot was cliche. Are you going to see it?
+John:'''
+#how many times have I told you not to leave your rock-climbing gear in the study? This is a space of learning and solitude, not an extreme sports store."'''
+text2 = '''Alex: "Hi, Bob!"
+
+Bob: "Hey, Alex. How's your day going?"
+
+Alex: "Not bad, thanks. Yours?"
+
+Bob:'''
+
+
+
+# %%
+print(len(model.to_str_tokens(text2)))
+# %%
+completion = model.generate(text2, max_new_tokens=100)
+print(completion)
+# %%
+def activation_to_disk(
+    attn_pattern: Float[torch.Tensor, "batch heads seqQ seqK"],
+    hook: HookPoint
+) -> Float[torch.Tensor, "batch heads seqQ seqK"]:
+
+    torch.save(attn_pattern, f"{hook.name}.pt")
+    return attn_pattern
+
+hooks = []
+for layer in range(model.cfg.n_layers):
+    hooks.append((f"blocks.{layer}.attn.hook_pattern", activation_to_disk))
+
+logits = model.run_with_hooks(text, fwd_hooks=hooks)
+
+# %%
+print(logits.shape)
